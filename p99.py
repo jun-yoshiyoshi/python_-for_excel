b4035d55f04f889efa49690d0e6483cd02dc0d9e
jun-yoshiyoshi <80947237+jun-yoshiyoshi@users.noreply.github.com>
2021-09-14 08:57:36 +0900

    Delete p99.py

--- p99.py
+++
@@ -1,8 +0,0 @@
-#列の幅と行の高さを変更する
-import openpyxl#openpyxlをインポート
-workBook=openpyxl.load_workbook
-("新宿店売上リスト.xlsx")#エクセルブックを開く
-sheet=workBook.active#アクティブなワークシートを選択
-sheet.column_demensions["A"].weidth=12#列の幅を設定(文字数)
-workBook.save('新宿店売上リスト(完成).xlsx')
-#ファイル名を指定して保存
