b6eb017fc5f95354741813c7a114e6cb17766062
jun-yoshiyoshi <80947237+jun-yoshiyoshi@users.noreply.github.com>
2021-09-14 08:57:58 +0900

    Delete p88.py

--- p88.py
+++
@@ -1,7 +0,0 @@
-#特定のデータを別のファイルに書き出し
-import pandas as pd#pandasをインポート
-df=pd.read_excel("店舗別売上リスト2.xlsx",sheet_name="Sheet1")#ブックを読み込み
-df=df[df.店名=="新宿店"]#新宿店のみ取得
-#シートごとにエクセルブックに書き出し
-with pd.ExcelWriter("新宿売上リスト.xlsx",date_format='YYYY/MM/DD',datetime_format='YYYY/MM/DD')as writer:
-	df.to_excel(writer,sheet_name="新宿店",index=False)
